5cd1d6347ab1c5559587d992514b67ec06bda127
Julian Simioni <julian@simioni.org>
2017-11-26 12:41:42 -0500

    First working debug version of docker image scripts

---
+++ files/build-script.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -exu
+
+# cd to workdir
+mkdir /gentoo
+cd /gentoo
+
+SUFFIX=${suffix:-} # set default
+STAGE3PATH="$(wget -q -O- "${DIST}/latest-stage3-${MICROARCH}${SUFFIX}.txt" | tail -n 1 | cut -f 1 -d ' ')"
+STAGE3="$(basename ${STAGE3PATH})"
+
+echo "Building Gentoo Container image for ${ARCH} ${SUFFIX} fetching from ${DIST}"
+
+# install required dependencies
+apk --no-cache add gnupg tar wget xz ca-certificates autoconf automake gettext gcc build-base libgcrypt-dev libgpg-error-dev libassuan-dev libksba-dev npth-dev gettext-dev
+
+
+wget -q -c "${DIST}/${STAGE3PATH}" "${DIST}/${STAGE3PATH}.CONTENTS" "${DIST}/${STAGE3PATH}.DIGESTS.asc"
+
+# verify downloaded stage 3 archive, using multiple keyservers if needed
+gpg --keyserver hkps.pool.sks-keyservers.net --recv-keys ${SIGNING_KEY} \
+ || gpg --keyserver keys.gnupg.net --recv-keys ${SIGNING_KEY} \
+ || gpg --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys ${SIGNING_KEY} \
+ && gpg --verify "${STAGE3}.DIGESTS.asc"
+
+
+awk '/# SHA512 HASH/{getline; print}' ${STAGE3}.DIGESTS.asc | sha512sum -c
+
+# extract stage3 archive
+tar xjpf "${STAGE3}" --xattrs --numeric-owner
+
+# tell Gentoo we are in a docker world (i think)
+sed -i -e 's/#rc_sys=""/rc_sys="docker"/g' etc/rc.conf \
+
+# set timezone
+echo 'UTC' > etc/timezone
+
+# cleanup
+rm ${STAGE3}.DIGESTS.asc ${STAGE3}.CONTENTS ${STAGE3}
+
+cd /root
+
+## build a static gpg to bootstrap authentication of snapshots within the next step
+GNUPG_VERSION=1.4.22
+wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-${GNUPG_VERSION}.tar.bz2
+wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-${GNUPG_VERSION}.tar.bz2.sig
+
+gpg --keyserver pgp.mit.edu --recv-keys 0x249B39D24F25E3B6
+#gpg --keyserver pgp.mit.edu --recv-keys 0x53B620D01CE0C630
+gpg --keyserver pgp.mit.edu --recv-keys 0x2071B08A33BD3F06
+gpg --verify gnupg-${GNUPG_VERSION}.tar.bz2.sig gnupg-${GNUPG_VERSION}.tar.bz2
+
+tar xvpf gnupg-${GNUPG_VERSION}.tar.bz2
+
+export CFLAGS="-static"
+export LDFLAGS="-static"
+cd gnupg-$GNUPG_VERSION
+./autogen.sh
+./configure --enable-static-rnd=linux
+nice make -j4
+
+# copy executable
+cp ./g10/gpg /gentoo/usr/bin
+# copy subprograms
+mkdir -p /gentoo/usr/local/libexec/gnupg
+cp ./keyserver/gpgkeys_curl /gentoo/usr/local/libexec/gnupg
+cp ./keyserver/gpgkeys_hkp /gentoo/usr/local/libexec/gnupg
--- gentoo-baseimage.Dockerfile
+++ gentoo-baseimage.Dockerfile
@@ -8,32 +8,44 @@ WORKDIR /
 ARG ARCH=amd64
 ARG MICROARCH=amd64
 ARG SUFFIX
-ARG DIST="http://distfiles.gentoo.org/releases/${ARCH}/autobuilds/"
+#ARG DIST="http://distfiles.gentoo.org/releases/${ARCH}/autobuilds/"
+ARG DIST="http://172.17.0.1:8000/releases/${ARCH}/autobuilds/"
 ARG SIGNING_KEY="0xBB572E0E2D182910"
 
-ARG SNAPSHOT="portage-latest.tar.xz"
-ARG SNAPSHOT_DIST="http://distfiles.gentoo.org/snapshots"
-ARG SNAPSHOT_SIGNING_KEY="0xEC590EEAC9189250"
-
-COPY build-script.sh /
+COPY files/build-script.sh /
 
 RUN /bin/sh /build-script.sh
 
+
+# take the stage3 downloaded in the alpine image and use it as a new container base
 FROM scratch
+
+ARG SNAPSHOT_NAME="portage-latest.tar.xz"
+#ARG SNAPSHOT_DIST="http://distfiles.gentoo.org/snapshots"
+ARG SNAPSHOT_DIST="http://172.17.0.1:8000/snapshots"
+ARG SNAPSHOT_SIGNING_KEY="0xEC590EEAC9189250"
 
 WORKDIR /
 COPY --from=builder /gentoo/ /
 
+RUN ls -lh /usr/bin/gpg
+RUN which gpg
+
 # install custom make.conf
-COPY make.conf /etc/portage/make.conf
+COPY files/make.conf /etc/portage/make.conf
 
-RUN emerge --sync
-
-# disable features that need ptrace during build
-RUN FEATURES="-sandbox -usersandbox" emerge -e world # rebuld the world :)
-
-RUN eselect news read # i hate this notification :P
-
+# make directories for custom config
 RUN mkdir -p /etc/portage/package.use /etc/portage/package.accept_keywords
+
+COPY files/emerge-wrapper.sh /
+
+# install gpg for real
+RUN /emerge-wrapper.sh app-crypt/gnupg -j2
+
+# emerge world, once with no security settings changes
+RUN /emerge-wrapper.sh -e world -j2
+
+# a few system things cannot be built in containers without this flag
+RUN FEATURES="-sandbox -usersandbox" /emerge-wrapper.sh --resume
 
 CMD ["/bin/bash"]
