6f50853caefce8f2dde43934c21b2fda17d942e7
Julian Simioni <julian@simioni.org>
2018-12-04 17:20:05 -0500

    Dual tag images

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 DATE=`date +%Y-%m-%d`
 
-docker build -t "orangejulius/gentoo-baseimage:$DATE" -f "gentoo-baseimage.Dockerfile" .
+docker build -t "orangejulius/gentoo-baseimage:$DATE" -t "orangejulius/gentoo-baseimage:latest" -f "gentoo-baseimage.Dockerfile" .
